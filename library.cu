fa9e67b47e038ea89a5942d49bbe2c3702563856
jalenlv <sejalenlv@mail.scut.edu.cn>
2024-04-24 01:45:24 +0800

    builtin types half way

--- library.cu
+++
@@ -1,6 +0,0 @@
-#include "library.cuh"
-#include <stdio.h>
-
-__global__ void hello() {
-  printf("Hello, world!\n");
-}
--- library.cuh
+++
@@ -1,6 +0,0 @@
-#ifndef CUDATEST_LIBRARY_CUH
-#define CUDATEST_LIBRARY_CUH
-
-__global__ void hello();
-
-#endif //CUDATEST_LIBRARY_CUH
