8d9e1bb42ce981700859a389ef208284964a4237
jalenlv <sejalenlv@mail.scut.edu.cn>
2024-04-28 01:20:42 +0800

    Update test file.

--- TEST.cu
+++ TEST.cu
@@ -4,39 +4,45 @@
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 
-#define N (1 << 14)
-#define MAX_NUMBER 5.0
-#define MIN_NUMBER -0.5
+#define N (1 << 22)
 
 int main() {
-  float *x = new float[N];
+  cuDoubleComplex *x = new cuDoubleComplex[N];
+  cuDoubleComplex *d_x;
+  for (size_t i = 0; i < N; i++) {
+    x[i] = make_cuDoubleComplex(-i, i);
+  }
+  cudaMalloc(&d_x, N * sizeof(cuDoubleComplex));
+  cudaMemcpy(d_x, x, N * sizeof(cuDoubleComplex), cudaMemcpyHostToDevice);
+
+  cuDoubleComplex *y = new cuDoubleComplex[N];
+  cuDoubleComplex *d_y;
+  for (size_t i = 0; i < N; i++) {
+    y[i] = make_cuDoubleComplex(2 * i, 2 * i);
+  }
+  cudaMalloc(&d_y, N * sizeof(cuDoubleComplex));
+  cudaMemcpy(d_y, y, N * sizeof(cuDoubleComplex), cudaMemcpyHostToDevice);
+
+  cuDoubleComplex alpha = make_cuDoubleComplex(2.0, -2.0);
+  cncblasZaxpy(N, &alpha, d_x, d_y);
+  cudaMemcpy(y, d_y, N * sizeof(cuDoubleComplex), cudaMemcpyDeviceToHost);
+  for (size_t i = 0; i < 10; i++) {
+    std::cout << y[i].x << ", " << y[i].y << std::endl;
+  }
 
   for (size_t i = 0; i < N; i++) {
-    x[i] = -1.0;
+    y[i] = make_cuDoubleComplex(2 * i, 2 * i);
   }
-  x[5678] = MIN_NUMBER;
-  x[1234] = MIN_NUMBER;
+  cudaMemset(d_y, 0, N * sizeof(cuDoubleComplex));
+  cudaMemcpy(d_y, y, N * sizeof(cuDoubleComplex), cudaMemcpyHostToDevice);
 
-  float *d_x;
-  cudaMalloc(&d_x, N * sizeof(float));
-  cudaMemcpy(d_x, x, N * sizeof(float), cudaMemcpyHostToDevice);
-
-  size_t result = cncblasSamin(N, d_x);
-
-  std::cout << "The index of the minimum element is: " << result << std::endl;
-
-  // use the official cublas library to verify the result
   cublasHandle_t handle;
   cublasCreate(&handle);
-  int cublas_result;
-  int min_index;
-  cublas_result = cublasIsamin(handle, N, d_x, 1, &min_index);
-  if (cublas_result != CUBLAS_STATUS_SUCCESS) {
-    std::cerr << "cublasIsamax failed" << std::endl;
-    return 1;
+  cublasZaxpy(handle, N, &alpha, d_x, 1, d_y, 1);
+  cudaMemcpy(y, d_y, N * sizeof(cuDoubleComplex), cudaMemcpyDeviceToHost);
+  for (size_t i = 0; i < 10; i++) {
+    std::cout << y[i].x << ", " << y[i].y << std::endl;
   }
-  std::cout << "The index of the minimum element is: " << min_index - 1 << std::endl;
-  cublasDestroy(handle);
 
   return 0;
 }
