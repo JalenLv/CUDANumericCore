a28a98a59fa27470f400b485d0f7d6891ae798ef
jalenlv <sejalenlv@mail.scut.edu.cn>
2024-04-30 00:46:03 +0800

    Add invalid argument checkers for AXPY

--- core/blas/src/axpy.cu
+++ core/blas/src/axpy.cu
@@ -1,4 +1,7 @@
-#include "level_one.cuh"
+#include <level_one.cuh>
+#include <cuComplex.h>
+#include <stdexcept>
+#include <iostream>
 
 /* -------------------- AXPY -------------------- */
 
@@ -12,6 +15,22 @@ __global__ void cncblasSaxpyKernel(size_t n, float alpha, const float *x, float 
 }
 
 void cncblasSaxpy(size_t n, const float *alpha, const float *x, float *y) {
+  // check if x and y are not null
+  try {
+    if (x == nullptr || y == nullptr) {
+      throw std::invalid_argument("x and y must not be null");
+    }
+    if (alpha == nullptr) {
+      throw std::invalid_argument("alpha must not be null");
+    }
+    if (n <= 0) {
+      throw std::invalid_argument("n must be greater than 0");
+    }
+  } catch (const std::invalid_argument &e) {
+    std::cerr << e.what() << std::endl;
+    exit(1);
+  }
+
   size_t num_blocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
   cncblasSaxpyKernel<<<num_blocks, BLOCK_SIZE>>>(n, *alpha, x, y);
 }
@@ -24,6 +43,22 @@ __global__ void cncblasDaxpyKernel(size_t n, double alpha, const double *x, doub
 }
 
 void cncblasDaxpy(size_t n, const double *alpha, const double *x, double *y) {
+  // check if x and y are not null
+  try {
+    if (x == nullptr || y == nullptr) {
+      throw std::invalid_argument("x and y must not be null");
+    }
+    if (alpha == nullptr) {
+      throw std::invalid_argument("alpha must not be null");
+    }
+    if (n <= 0) {
+      throw std::invalid_argument("n must be greater than 0");
+    }
+  } catch (const std::invalid_argument &e) {
+    std::cerr << e.what() << std::endl;
+    exit(1);
+  }
+
   size_t num_blocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
   cncblasDaxpyKernel<<<num_blocks, BLOCK_SIZE>>>(n, *alpha, x, y);
 }
@@ -36,6 +71,22 @@ __global__ void cncblasCaxpyKernel(size_t n, cuComplex alpha, const cuComplex *x
 }
 
 void cncblasCaxpy(size_t n, const cuComplex *alpha, const cuComplex *x, cuComplex *y) {
+  // check if x and y are not null
+  try {
+    if (x == nullptr || y == nullptr) {
+      throw std::invalid_argument("x and y must not be null");
+    }
+    if (alpha == nullptr) {
+      throw std::invalid_argument("alpha must not be null");
+    }
+    if (n <= 0) {
+      throw std::invalid_argument("n must be greater than 0");
+    }
+  } catch (const std::invalid_argument &e) {
+    std::cerr << e.what() << std::endl;
+    exit(1);
+  }
+
   size_t num_blocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
   cncblasCaxpyKernel<<<num_blocks, BLOCK_SIZE>>>(n, *alpha, x, y);
 }
@@ -48,6 +99,22 @@ __global__ void cncblasZaxpyKernel(size_t n, cuDoubleComplex alpha, const cuDoub
 }
 
 void cncblasZaxpy(size_t n, const cuDoubleComplex *alpha, const cuDoubleComplex *x, cuDoubleComplex *y) {
+  // check if x and y are not null
+  try {
+    if (x == nullptr || y == nullptr) {
+      throw std::invalid_argument("x and y must not be null");
+    }
+    if (alpha == nullptr) {
+      throw std::invalid_argument("alpha must not be null");
+    }
+    if (n <= 0) {
+      throw std::invalid_argument("n must be greater than 0");
+    }
+  } catch (const std::invalid_argument &e) {
+    std::cerr << e.what() << std::endl;
+    exit(1);
+  }
+
   size_t num_blocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
   cncblasZaxpyKernel<<<num_blocks, BLOCK_SIZE>>>(n, *alpha, x, y);
 }
